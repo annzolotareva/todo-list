d3da990b4fcf5cdd6d4f793b8158cc9e1e0a12d8
Золотарева Анна Александровна <aazolotareva@phoenixit.ru>
2022-09-20 04:09:12 +0300

    первый коммит проекта

---
+++ src/app/components/list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'todo-list',
+    templateUrl: 'list.component.html'
+})
+
+export class ListComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
